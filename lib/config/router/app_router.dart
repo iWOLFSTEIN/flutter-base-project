b38c9a9c6a0b4b2f06d910d3154cf5872e694e76
Talha Ashraf <talha.ashraf@brainxtech.com>
2024-08-24 01:10:12 +0500

    minor change

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -28,123 +28,19 @@ class AppRouter {
         routes: [
           GoRoute(
             path: home.path,
-            pageBuilder: (context, state) => SlideTransitionPage(
-              previousIndex:
-                  navigationStack.isEmpty ? -1 : navigationStack.last,
-              nextIndex: home.id!,
-              child: const Home(),
-            ),
+            pageBuilder: (context, state) =>
+                const NoTransitionPage(child: Home()),
           ),
           GoRoute(
-            path: explore.path,
-            pageBuilder: (context, state) => SlideTransitionPage(
-              previousIndex: navigationStack.last,
-              nextIndex: explore.id!,
-              child: const Explore(),
-            ),
-          ),
+              path: explore.path,
+              pageBuilder: (context, state) =>
+                  const NoTransitionPage(child: Explore())),
           GoRoute(
-            path: settings.path,
-            pageBuilder: (context, state) => SlideTransitionPage(
-              previousIndex: navigationStack.last,
-              nextIndex: settings.id!,
-              child: const Settings(),
-            ),
-          ),
+              path: settings.path,
+              pageBuilder: (context, state) =>
+                  const NoTransitionPage(child: Settings())),
         ],
       ),
     ],
   );
-}
-
-// class SlideTransitionPage<T> extends CustomTransitionPage<T> {
-
-//   final Widget child;
-
-//   SlideTransitionPage({required this.child})
-//       : super(
-//           transitionDuration: const Duration(milliseconds: 300),
-//           reverseTransitionDuration: const Duration(milliseconds: 300),
-//           child: child,
-//           transitionsBuilder: (context, animation, secondaryAnimation, child) {
-//             const begin = Offset(1.0, 0.0);
-//             const end = Offset.zero;
-//             const curve = Curves.easeInOut;
-
-//             final tween =
-//                 Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-//             return SlideTransition(
-//               position: animation.drive(tween),
-//               child: child,
-//             );
-//           },
-//         );
-// }
-
-// class SlideTransitionPage<T> extends CustomTransitionPage<T> {
-//   final Widget child;
-
-//   SlideTransitionPage({required this.child})
-//       : super(
-//           transitionDuration: const Duration(milliseconds: 300),
-//           reverseTransitionDuration: const Duration(milliseconds: 300),
-//           child: child,
-//           transitionsBuilder: (context, animation, secondaryAnimation, child) {
-//             const forwardBegin = Offset(1.0, 0.0);
-//             const forwardEnd = Offset.zero;
-//             const reverseBegin = Offset.zero;
-//             const reverseEnd = Offset(-1.0, 0.0);
-//             const curve = Curves.easeInOut;
-
-//             final forwardTween = Tween(begin: forwardBegin, end: forwardEnd)
-//                 .chain(CurveTween(curve: curve));
-//             final reverseTween = Tween(begin: reverseBegin, end: reverseEnd)
-//                 .chain(CurveTween(curve: curve));
-
-//             final offsetAnimation = animation.drive(forwardTween);
-//             final reverseOffsetAnimation =
-//                 secondaryAnimation.drive(reverseTween);
-
-//             return SlideTransition(
-//               position: offsetAnimation,
-//               child: SlideTransition(
-//                 position: reverseOffsetAnimation,
-//                 child: child,
-//               ),
-//             );
-//           },
-//         );
-// }
-
-class SlideTransitionPage<T> extends CustomTransitionPage<T> {
-  final Widget child;
-  final int previousIndex;
-  final int nextIndex;
-
-  SlideTransitionPage({
-    required this.child,
-    required this.previousIndex,
-    required this.nextIndex,
-  }) : super(
-          transitionDuration: const Duration(milliseconds: 300),
-          reverseTransitionDuration: const Duration(milliseconds: 300),
-          child: child,
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            final begin = nextIndex < previousIndex
-                ? const Offset(-1.0, 0.0)
-                : const Offset(1.0, 0.0);
-            const end = Offset.zero;
-            const curve = Curves.easeInOut;
-
-            final tween =
-                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-            final offsetAnimation = animation.drive(tween);
-
-            return SlideTransition(
-              position: offsetAnimation,
-              child: child,
-            );
-          },
-        );
 }
